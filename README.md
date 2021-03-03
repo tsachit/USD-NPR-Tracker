3910e71202c62dd8e473738251cae048a9a48cbb
Sachit Karki <sachitjungkarki@gmail.com>
2021-03-03 21:36:07 +0545

    push notification

--- README.md
+++ README.md
@@ -3,15 +3,32 @@
 Just a simple tracker from **Transferwise** to fetch USD to NPR rate in every 5 minutes.
 
 ### Setup
+Note: Only if you want to use venv
 ```
 > python -m venv venv
 > source venv/bin/activate
-> pip install -r requirements.txt
+```
+
+install all the dependency using this
+```
+pip install -r requirements.txt
 ```
 
 Change interval value in `tracker.py` to increase/decrease the frequency.
 
 ### How to run
+If you're using venv then activate it
+```
+source venv/bin/activate
+```
+and run tracker command
 ```
 python tracker.py
 ```
+
+If you also want alert on above threshold value, try running like this
+
+```
+python tracker.py 117
+```
+This will send you push notification if USD-NPR rate is above Rs.117
--- tracker.py
+++ tracker.py
@@ -1,12 +1,20 @@
+import pync
 import requests
+import sys
 import sched, time
 from datetime import datetime
 
 # run interval in seconds
 interval = 300
+url = "https://api.transferwise.com/v3/comparisons?sourceCurrency=USD&targetCurrency=NPR&sendAmount=1"
+
+def notify(rate):
+    if(len(sys.argv) > 1):
+        threshold = float(sys.argv[1])
+        if(rate > threshold):
+            pync.Notifier.notify(f"USD to NPR rate is now Rs.{rate}, i.e. above the limit Rs.{threshold}", title='USD Tracker')
 
 def get_rate():
-    url = "https://api.transferwise.com/v3/comparisons?sourceCurrency=USD&targetCurrency=NPR&sendAmount=1"
     response = requests.request("GET", url, headers={}, data={})
     json_resp = response.json()
     return json_resp['providers'][0]['quotes'][0]['rate']
@@ -18,8 +26,10 @@ print("*********************************************************")
 # do your stuff
 print("Time \t\t\t\t\t Rate")
 s = sched.scheduler(time.time, time.sleep)
-def do_something(sc): 
-    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} \t\t\t ${get_rate()}")
+def do_something(sc):
+    rate = get_rate()
+    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} \t\t\t Rs.{rate}")
+    notify(rate)
     s.enter(interval, 1, do_something, (sc,))
 
 # run instantly
